f44ff776b5cb2955a9338d82e484165c1b7b6752
Sam Fuller <spfuller01@gmail.com>
2019-08-27 10:21:21 -0500

    using yarn and adding license

--- routes/api/rest.js
+++ routes/api/rest.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const random = require('random-name')
+const express = require('express');
+const router = express.Router();
+const random = require('random-name');
 
 // Basic REST endpoint. Returns a random full name
 router.get('/', (req, res) => {
   res.status(200).json({
     first_name: random.first(),
     middle_name: random.middle(),
-    last_name: random.last()
-  })
-})
+    last_name: random.last(),
+    random_num: Math.random()
+  });
+});
 
 module.exports = router
--- routes/graphql/graphql.js
+++ routes/graphql/graphql.js
@@ -1,21 +1,23 @@
-const { buildSchema } = require('graphql')
-const random = require('random-name')
+const { buildSchema } = require('graphql');
+const random = require('random-name');
 
 const schema = buildSchema(`
   type Query {
     first_name: String,
     middle_name: String,
-    last_name: String
+    last_name: String,
+    random_number: Float
   }
-`)
+`);
 
 const root = {
   first_name: random.first(),
   middle_name: random.middle(),
-  last_name: random.last()
-}
+  last_name: random.last(),
+  random_number: Math.random()
+};
 
 module.exports = {
   schema,
   root
-}
+};
